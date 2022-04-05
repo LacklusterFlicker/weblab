3647c495d13d1504800da5f80bc13ed41ec8886c
LacklusterFlicker <97092959+LacklusterFlicker@users.noreply.github.com>
2022-04-05 13:50:54 +0300

    Add files via upload

--- calc.php
+++ calc.php
@@ -1,18 +1,53 @@
 <!-- Область основного контента -->
-<form action=''>
-  <label>Число 1:</label>
-  <br />
-  <input name='num1' type='text' />
-  <br />
-  <label>Оператор: </label>
-  <br />
-  <input name='operator' type='text' />
-  <br />
-  <label>Число 2: </label>
-  <br />
-  <input name='num2' type='text' />
-  <br />
-  <br />
-  <input type='submit' value='Считать'>
+<?php
+if (isset($_POST['submit'])) {
+    $num1 = (int) $_POST['num1'];
+    $operator = $_POST['operator'];
+    $num2 = (int) $_POST['num2'];
+
+    if ($operator != '+' && $operator != '-' && $operator != '*' && $operator != '/') {
+        $result = 'Вы ввели неправильный оператор...';
+    } else {
+        switch ($operator) {
+            case '+':
+                $result = 'Результат: ' . $num1 + $num2;
+                break;
+            case '-':
+                $result = 'Результат: ' . $num1 - $num2;
+                break;
+            case '*':
+                $result = 'Результат: ' . $num1 * $num2;
+                break;
+            case '/':
+                if ($num2 == 0) {
+                    $result = 'На 0 делить нельзя.';
+                } else {
+                    $result = 'Результат: ' . $num1 / $num2;
+                }
+                break;
+            default:
+                $result = 'Вы ввели неправильный оператор';
+        }
+    }
+    echo $result;
+}
+?>
+<br>
+<br>
+<form action='' method="POST">
+    <label>Число 1:</label>
+    <br />
+    <input name='num1' type='number' required />
+    <br />
+    <label>Оператор: </label>
+    <br />
+    <input name='operator' type='text' required />
+    <br />
+    <label>Число 2: </label>
+    <br />
+    <input name='num2' type='number' required />
+    <br />
+    <br />
+    <input type='submit' name="submit" value='Считать'>
 </form>
 <!-- Область основного контента -->
